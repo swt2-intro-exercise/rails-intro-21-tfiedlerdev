acd43fb35b48ae77a31b55d9affeb748033c38ce
Tobias Fiedler <tfiedlerdevelopment@gmail.com>
2021-11-08 11:22:45 +0100

    Added failing test for #8

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ApplicationRecord
+    
     def name() 
         return self.first_name + " " +self.last_name
     end 
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe Author, type: :model do
     expect(author.homepage).to eq "http://wikipedia.org/Alan_Turing"
     expect(author.name).to eq "Alan Touring"
     end
+  it "should not validate without last name" do
+    author = Author.new({first_name: "Alan",  homepage:"http://wikipedia.org/Alan_Turing"})
+    expect(author).to_not be_valid
+  end
 end
