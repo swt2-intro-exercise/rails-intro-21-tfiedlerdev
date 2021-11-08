82350490acf7ae4ebed491adb252746e16e08f4a
Tobias Fiedler <tfiedlerdevelopment@gmail.com>
2021-11-08 10:51:43 +0100

    Issue #6 resolved

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,6 +8,9 @@ class AuthorsController < ApplicationController
         @author.save
         redirect_to root_path, notice: 'Success!'
   end
+  def show
+    @author = Author.find(params[:id])
+  end
   private
   def author_params
     params.require(:author).permit(:first_name, :last_name, :homepage)
--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 describe "Show author page", type: :feature do
-  it "should display an authors details" do
+  it "should display an authors details (should exist at 'author_path(:id)' and render without an error)" do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
-    visit author_path(@alan)
+    author = create :author
+    visit author_path(author.id)
     expect(page).to have_text("Alan")
     expect(page).to have_text("Turing")
     expect(page).to have_text("http://wikipedia.de/Alan_Turing")
