912fb5c0b9f53e222415205174b84ebe531a2c2e
Tobias Fiedler <tfiedlerdevelopment@gmail.com>
2021-11-08 12:59:54 +0100

    Added failing test for #14

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not validate without all attributes" do
+    author1 = Paper.new({year: 0})
+    expect(author1).to_not be_valid
+    author2 = Paper.new({title: "Hallo"})
+    expect(author2).to_not be_valid
+    author3 = Paper.new({venue: "Test"})
+    expect(author3).to_not be_valid
+  end
+  it "should not validate with attributes of wrong type" do
+    author = Paper.new({year: "Hallo", venue: "HPI", title: "Hassos Paper"})
+    expect(author).to_not be_valid
+   
+  end
 end
