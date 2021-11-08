98d8135c2c97d7d5f996568d16ca0d4a1d8335dc
Tobias Fiedler <tfiedlerdevelopment@gmail.com>
2021-11-08 11:59:27 +0100

    Added failing test for #10

---
+++ spec/features/author/edit_author_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "Edit author page", type: :feature do
+  it "should exist at 'edit_author_path' and render withour error" do
+    author = create :author
+    visit edit_author_path(author.id)
+
+  end
+  it "should store changes in the database" do
+    author = create :author
+    visit edit_author_path(author.id)
+    page.fill_in 'author[last_name]', with: 'Dijkstra'
+    find('input[type="submit"]').click
+    author.reload
+    expect(author.last_name).to eq 'Dijkstra'
+    
+  end
+end
