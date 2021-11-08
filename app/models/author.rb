651ac89096c190eb28c637df5f0bed7219f6b07f
Tobias Fiedler <tfiedlerdevelopment@gmail.com>
2021-11-08 11:23:16 +0100

    Fixed #8

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ApplicationRecord
-    
+    validates :last_name, presence: true,
+                    length: { minimum: 1 }
     def name() 
         return self.first_name + " " +self.last_name
     end 
