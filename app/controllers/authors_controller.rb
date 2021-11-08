7bab05659750a0e323d6b7b3e2a1c62b06071125
Tobias Fiedler <tfiedlerdevelopment@gmail.com>
2021-11-08 12:34:07 +0100

    Fixed failing test for #12

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -31,6 +31,12 @@ class AuthorsController < ApplicationController
       render 'edit'
     end
   end
+  def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+ 
+    redirect_to authors_path
+  end
   private
   def author_params
     params.require(:author).permit(:first_name, :last_name, :homepage)
