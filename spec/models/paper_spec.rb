64f3d4a9df08c00d814d2e70d6f21e4147092539
Tobias Fiedler <tfiedlerdevelopment@gmail.com>
2021-11-13 11:17:48 +0100

    Added failing test for #15

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,16 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
   it "should not validate without all attributes" do
-    author1 = Paper.new({year: 0})
-    expect(author1).to_not be_valid
-    author2 = Paper.new({title: "Hallo"})
-    expect(author2).to_not be_valid
-    author3 = Paper.new({venue: "Test"})
-    expect(author3).to_not be_valid
+    paper1 = Paper.new({year: 0})
+    expect(paper1).to_not be_valid
+    paper2 = Paper.new({title: "Hallo"})
+    expect(paper2).to_not be_valid
+    paper3 = Paper.new({venue: "Test"})
+    expect(paper3).to_not be_valid
   end
   it "should not validate with attributes of wrong type" do
-    author = Paper.new({year: "Hallo", venue: "HPI", title: "Hassos Paper"})
-    expect(author).to_not be_valid
-   
+    paper = Paper.new({year: "Hallo", venue: "HPI", title: "Hassos Paper"})
+    expect(paper).to_not be_valid
+  end
+  it "should have a list of authors" do
+    paper = Paper.new({year: "Hallo", venue: "HPI", title: "Hassos Paper"})
+    expect(paper).to include(:authors)
   end
 end
