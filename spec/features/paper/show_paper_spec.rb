9895cfd669173668ab958086048e9ac034bcbc0a
Tobias Fiedler <tfiedlerdevelopment@gmail.com>
2021-11-13 12:17:34 +0100

    Added missing test file for  #16

---
+++ spec/features/paper/show_paper_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Show paper page", type: :feature do
+  it "should display all associated authors" do
+    #paper = create :paper
+    #paper = Paper.new({year: "Hallo", venue: "HPI", title: "Hassos Paper", id:0})
+    paper = create :paper
+    author = create :author
+    paper.authors<< author
+    visit paper_path(paper.id)
+    expect(page).to have_text("Alan Touring")
+  end
+end
